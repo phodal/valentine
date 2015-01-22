775983362b30d553b4f25a6ca9752f252afa4a80
Fengda HUANG <h@phodal.com>
2015-01-22 13:09:57 +0800

    simple restful api

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # valentina
+
+
+
+##Remind
+
+    curl http://0.0.0.0:5000/todo1 -d "data=Remember the milk" -X PUT
--- server.py
+++ server.py
@@ -1,13 +1,29 @@
-from flask import Flask, flash, render_template
-from flask.ext import restful
-from flask_restful import reqparse, Resource
+from flask import Flask, request
+from flask_restful import Resource, Api
 
 app = Flask(__name__, static_url_path='')
 app.secret_key = 'why would I tell you my secret key?'
+api = Api(app)
+tasks = {}
+
 
 @app.route('/')
 def root():
     return app.send_static_file('index.html')
+
+
+class Task(Resource):
+    @staticmethod
+    def get(task_id):
+        return {task_id: tasks[task_id]}
+
+    @staticmethod
+    def put(task_id):
+        tasks[task_id] = request.form['data']
+        return {task_id: tasks[task_id]}
+
+
+api.add_resource(Task, '/<string:task_id>')
 
 if __name__ == '__main__':
     app.run(debug=True)
