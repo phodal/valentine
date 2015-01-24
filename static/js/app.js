4ae4615f507f0b6fb1bfe47f0c6d41f2569aca4f
Fengda HUANG <h@phodal.com>
2015-01-24 16:11:56 +0800

    modify style and text

--- static/js/app.js
+++ static/js/app.js
@@ -2,18 +2,18 @@
     var L = new lettuce();
 
     var data = {
-        about: "Dream",
-        what: "More Dream"
+        rise: "一起看日出",
+        down: "一起看日落"
     };
 
-    var about = function () {
-	    var html = '<div class="rise"><h3>{%=o.about%}</h3></div>';
+    var rise = function () {
+	    var html = '<div class="rise"><h3>{%=o.rise%}</h3></div>';
 	    var result = L.Template.tmpl(html, data);
         document.getElementById("results").innerHTML = result;
     };
 
-    function what() {
-	    var html = '<div class="down"><h3>{%=o.what%}</h3></div>';
+    function down() {
+	    var html = '<div class="down"><h3>{%=o.down%}</h3></div>';
         var result = L.Template.tmpl(html, data);
         document.getElementById("results").innerHTML = result;
     }
@@ -28,7 +28,7 @@
 		setInterval(function(){a.fillStyle="rgba(0,0,0,.2)";a.fillRect(0,0,O,Q);for(i=v;i--;){f=e[i];u=f[0];q=h[u.q];D=u.x-q[0];E=u.y-q[1];G=M.sqrt(D*D+E*E);10>G&&(0.95<R()?u.q=~~(R()*v):(0.99<R()&&(u.D*=-1),u.q+=u.D,u.q%=v,0>u.q&&(u.q+=v)));u.X+=-D/G*u.S;u.Y+=-E/G*u.S;u.x+=u.X;u.y+=u.Y;_(u);u.X*=u.F;u.Y*=u.F;for(k=0;k<v-1;)T=f[k],N=f[++k],N.x-=0.7*(N.x-T.x),N.y-=0.7*(N.y-T.y),_(N)}},25);
 	}
 
-    function why() {
+    function final() {
 	    document.getElementById("results").innerHTML = '<canvas width="1440" height="740"></canvas>';
 	    lettuce.post("/task", "irsend SEND_ONCE home KEY_0");
 	    L.Event.trigger("showLove")
@@ -42,16 +42,15 @@
 
     L.Router
         .add(/#/, home)
-        .add(/#about/, about)
-        .add(/#what/, what)
-        .add(/#why/, why)
+        .add(/#about/, rise)
+        .add(/#what/, down)
+        .add(/#why/, final)
         .load();
 
 	function late(func, n){
 		var p = new L.Promise();
 		setTimeout(function() {
 			if(func !== undefined){
-				//L.FX.fadeOut(document.getElementById('results'), {duration: 3000, complete: function() {}});
 				func();
 				L.FX.fadeIn(document.getElementById('results'), {duration: 3000, complete: function() {}});
 			}
@@ -63,15 +62,15 @@
 	var p = new L.Promise();
 	late(undefined, 3000).then(
 		function() {
-			return late(about, 0)
+			return late(rise, 0)
 		}
 	).then(
 		function() {
-			return late(what, 3000)
+			return late(down, 3000)
 		}
 	).then(
 		function() {
-			return late(why, 3000)
+			return late(final, 3000)
 		}
 	)
 
